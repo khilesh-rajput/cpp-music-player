ec26535d4aef3c7142f2706b148efcbf4df19978
Khilesh Rajput <khileshrajput2005@gmail.com>
2025-08-29 18:22:08 +0530

    added prev next and add fnx

--- playlist.cpp
+++ playlist.cpp
@@ -1,13 +1,69 @@
 #include "playlist.h"
-#include <string>
-struct Song{
-    std::string title;
-    Song* prev;
-    Song* next;
+#include <iostream>
+using namespace std;
 
-    Song(std::string name){
-        title = name;
-        prev = nullptr;
-        next = nullptr;
+Playlist:: Playlist(){
+    head = nullptr;
+    tail = nullptr;
+    current = nullptr;
+}
+
+void Playlist::addSong() {
+    string name;
+    cout << "Enter song name : ";
+    cin.ignore();
+    getline(cin,name);
+
+    Song* newSong = new Song(name);
+
+    if(head == nullptr){
+        head = newSong;
+        tail = newSong;
+        current = newSong;
+    }else{
+        tail->next = newSong;
+        newSong->prev = tail;
+        tail = newSong;
     }
-};
+}
+
+
+void Playlist::playNext(){
+
+    if(current == nullptr) {
+        cout << "Playlist is empty.\n";
+        return;
+    }
+
+    if(current->next == nullptr){
+        cout << "this is the last song of the playlist, add more songs" << endl;
+        return;
+    }else{
+        current = current->next;
+    }
+    cout << "Now playing: " << current->title << endl;
+}
+
+void Playlist::playPrev(){
+
+    if(current == nullptr) {
+        cout << "Playlist is empty" << endl;
+        return;
+    }
+
+    if(current->prev == nullptr){
+        cout << "this is first song of the playlist";
+    }else{
+        current = current->prev;
+        cout << "Now playing: " << current->title << endl;
+    }
+}    
+
+void Playlist::deleteSong(){
+    cout << "not implemented yet";
+}
+
+void Playlist::displayPlaylist(){
+    cout << "not implemented yet";
+}
+
--- playlist.h
+++ playlist.h
@@ -1,12 +1,28 @@
+#include <string>
+
+struct Song{
+    std::string title;
+    Song* prev;
+    Song* next;
+
+    Song(std::string name) {
+        title = name;
+        prev = nullptr;
+        next = nullptr;
+    }
+};
 class Playlist{
+
+    private:
+    Song* head;
+    Song* tail;
+    Song* current;
+
     public:
-    void addSong(){};
-
-    void deleteSong(){};
-
-    void displayPlaylist(){};
-
-    void playNext(){};
-
-    void playPrev(){};
+    Playlist();
+    void addSong();
+    void deleteSong();
+    void displayPlaylist();
+    void playNext();
+    void playPrev();
 };
